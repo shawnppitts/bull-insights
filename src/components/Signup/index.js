5219742904de5e1ff320cb284ead9dcf0d98c2b3
shawnppitts <shawnppitts@gmail.com>
2021-09-16 16:04:18 -0400

    Build Signup / Login UI Components

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,7 +2,37 @@ import React, { Component } from "react";
 import { Form, Input, Button } from 'semantic-ui-react'
 import './index.css';
 
-class Signup extends Component{  
+class Signup extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            fullName: "",
+            username: "",
+            password: ""
+        }
+        this.setFullname = this.setFullname.bind(this);        
+        this.setUsername = this.setUsername.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+        this.register = this.register.bind(this);
+    }
+
+    setFullname(event){
+        this.setState({fullName: event.target.value});
+    }
+
+    setUsername(event){
+        this.setState({username: event.target.value});
+    }
+
+    setPassword(event){
+        this.setState({password: event.target.value});
+    }
+
+    register(){
+        console.log(this.state);
+    } 
+
     render(){
         return (
             <div className="flex-wrapper">
@@ -10,18 +40,18 @@ class Signup extends Component{
                     <h1 className="flex-header">Sign Up</h1>
                     <Form.Field required>
                         <label className="input-label">Full Name</label>
-                        <Input className="login-inputs" placeholder='Enter Name' />
+                        <Input onChange={this.setFullname} className="login-inputs" placeholder='Enter Name' />
                     </Form.Field>                                        
                     <Form.Field required>
                         <label className="input-label">Username</label>
-                        <Input className="login-inputs" placeholder='Enter Username' />
+                        <Input onChange={this.setUsername} className="login-inputs" placeholder='Enter Username' />
                     </Form.Field>
                     <Form.Field required>
                         <label className="input-label">Password</label>
-                        <Input className="login-inputs" type="password" placeholder='Enter Password' />
+                        <Input onChange={this.setPassword} className="login-inputs" type="password" placeholder='Enter Password' />
                     </Form.Field>
                     <div id="submit-btn-wrapper"> 
-                        <Button id="submit" size="big" type='submit'>Signup</Button>
+                        <Button onClick={this.register} id="submit" size="big" type='submit'>Signup</Button>
                     </div>                   
                 </Form>
             </div>
