48c49062a00915d707ddfe9da2e59c1a20d2d35e
shawnppitts <shawnppitts@gmail.com>
2021-09-15 22:59:25 -0400

    UI restructuring

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
+import { Form, Input, Button } from 'semantic-ui-react'
 import './index.css';
 
 class Signup extends Component{  
     render(){
         return (
-            <div className="login-wrapper">
-                <h1>Signup:</h1>
-                <form>
-                    <label>
-                        <p>Username:</p>
-                        <input type="text" />
-                    </label>
-                    <label>
-                        <p>Password:</p>
-                        <input type="password" />
-                    </label>
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
+            <div className="flex-wrapper">
+                <Form>
+                    <h1 className="flex-header">Sign Up</h1>
+                    <Form.Field required>
+                        <label className="input-label">Full Name</label>
+                        <Input className="login-inputs" placeholder='Enter Name' />
+                    </Form.Field>                                        
+                    <Form.Field required>
+                        <label className="input-label">Username</label>
+                        <Input className="login-inputs" placeholder='Enter Username' />
+                    </Form.Field>
+                    <Form.Field required>
+                        <label className="input-label">Password</label>
+                        <Input className="login-inputs" type="password" placeholder='Enter Password' />
+                    </Form.Field>
+                    <div id="submit-btn-wrapper"> 
+                        <Button id="submit" size="big" type='submit'>Signup</Button>
+                    </div>                   
+                </Form>
             </div>
         );
     }    
